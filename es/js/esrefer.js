306f4dca5132f6793ed3deaa0be2ef5ddabbd83c
baidonghui <baidonghui@live.cn>
2015-08-07 22:11:01 +0800

    Site updated: 2015-08-07 22:10:58

---
+++ es/js/esrefer.js
@@ -0,0 +1,4 @@
+if(String(document.location.href).substr(7,3) == "es.")
+{
+	document.location.href="/es";		
+}
---
+++ js/esrefer.js
@@ -0,0 +1,1 @@
+if(String(document.location.href).substr(7,3)=="es."){document.location.href="/es"}
