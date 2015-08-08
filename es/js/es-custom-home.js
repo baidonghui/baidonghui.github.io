f995af82be755466ad725c63dcccca6b4b3cdbdf
baidonghui <baidonghui@live.cn>
2015-08-08 20:22:53 +0800

    Site updated: 2015-08-08 20:22:52

--- es/js/es-custom-home.js
+++ es/js/es-custom-home.js
@@ -1,15 +1,8 @@
 function GetQueryString(name) {
-	console.info(window.location);
-	console.info(decodeURI(window.location));
-
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
 	var r = window.location.search.substr(1).match(reg);
-
-
-	console.info(r);
-
 	if (r != null) return decodeURI(r[2]);
-	return "http://www.baidu.com";;
+	return "http://www.baidu.com";
 }
 
 $(document).ready(function() {
